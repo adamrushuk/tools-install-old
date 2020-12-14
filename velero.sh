fd6636bbce306a40b6b4b0004f4857026b0e32b5
Adam Rush <adamrushuk@gmail.com>
2020-12-14 08:18:35 +0000

    Added velero script

---
+++ velero.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+DIR=$(cd `dirname $0` && pwd)
+source $DIR/.lib.sh
+
+ver="v1.5.2"
+start "Velero $ver"
+
+curl -L "https://github.com/vmware-tanzu/velero/releases/download/${ver}/velero-${ver}-linux-amd64.tar.gz" -o /tmp/velero.tar.gz
+
+cd /tmp
+mkdir -p  $HOME/.local/bin
+
+tar -zxf velero.tar.gz
+sudo mv velero-${ver}-linux-amd64/velero $HOME/.local/bin
+
+rm -rf velero-${ver}-linux-amd64
+rm velero.tar.gz
+
+end 'velero' '--version'
