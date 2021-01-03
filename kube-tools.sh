7d0b25ff39d7129b2a184ab80dc6da6bbedde68d
Adam Rush <adamrushuk@gmail.com>
2021-01-03 18:11:12 +0000

    Added krew

--- kube-tools.sh
+++ kube-tools.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 set -e
-DIR=$(cd `dirname $0` && pwd)
+DIR=$(cd $(dirname $0) && pwd)
 source $DIR/.lib.sh
 
 start "kubens & kubectx"
 
-mkdir -p  $HOME/.local/bin
+mkdir -p $HOME/.local/bin
+
+# install krew
+(
+    set -x
+    cd "$(mktemp -d)" &&
+        curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
+        tar zxvf krew.tar.gz &&
+        KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/')" &&
+        "$KREW" install krew
+)
+
+kubectl krew list
+kubectl krew install ctx
+kubectl krew install ns
 
 curl -s https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens > ~/.local/bin/kubens
 chmod u+x ~/.local/bin/kubens
