0304c5919a376c6bf4884a883c43d8851b524ed6
Adam Rush <adamrushuk@gmail.com>
2021-02-14 08:21:34 +0000

    Added Argo CD tool

---
+++ argocd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+DIR=$(cd "$(dirname "$0")" && pwd)
+source "$DIR/.lib.sh"
+
+# https://github.com/argoproj/argo-cd/releases
+version="1.8.4"
+start "Argo CD $version"
+
+mkdir -p  "$HOME/.local/bin"
+
+sudo curl -sSL -o "$HOME/.local/bin/argocd" https://github.com/argoproj/argo-cd/releases/download/v${version}/argocd-linux-amd64
+sudo chmod +x "$HOME/.local/bin/argocd"
+
+end 'argocd' 'version'
