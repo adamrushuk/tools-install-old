e25466948d01696c76ac4bc355b41e5166cd3485
Adam Rush <adamrushuk@gmail.com>
2021-10-16 08:28:20 +0100

    Changed bicep to v0.4.1008

--- bicep.sh
+++ bicep.sh
@@ -4,7 +4,7 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 source "$DIR/.lib.sh"
 
 # https://github.com/Azure/bicep/releases/
-VER="0.4.613"
+VER="0.4.1008"
 start "bicep $VER"
 
 mkdir -p  "$HOME/.local/bin"
