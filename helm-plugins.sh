e1f5a16c36a173f4b03dc7b80369333e1a6d8e0e
Adam Rush <adamrushuk@gmail.com>
2022-01-10 08:13:35 +0000

    Added idempotent step

--- helm-plugins.sh
+++ helm-plugins.sh
@@ -5,9 +5,11 @@ helm plugin list
 echo
 
 # https://github.com/databus23/helm-diff/releases
-helm plugin install https://github.com/databus23/helm-diff --version v3.2.0
+helm plugin uninstall diff 2>/dev/null
+helm plugin install https://github.com/databus23/helm-diff --version v3.3.0
 
 # https://github.com/helm/helm-mapkubeapis/releases
+helm plugin uninstall mapkubeapis 2>/dev/null
 helm plugin install https://github.com/helm/helm-mapkubeapis --version v0.1.0
 
 echo -e "\nHelm Plugins:"
