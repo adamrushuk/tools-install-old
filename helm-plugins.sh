740835873fa9985325355edb9f0b22b9368e183b
Adam Rush <adamrushuk@gmail.com>
2022-02-13 08:13:09 +0000

    Bumped helm tools

--- helm-plugins.sh
+++ helm-plugins.sh
@@ -6,11 +6,11 @@ echo
 
 # https://github.com/databus23/helm-diff/releases
 helm plugin uninstall diff 2>/dev/null
-helm plugin install https://github.com/databus23/helm-diff --version v3.3.0
+helm plugin install https://github.com/databus23/helm-diff --version v3.4.2
 
 # https://github.com/helm/helm-mapkubeapis/releases
 helm plugin uninstall mapkubeapis 2>/dev/null
-helm plugin install https://github.com/helm/helm-mapkubeapis --version v0.1.0
+helm plugin install https://github.com/helm/helm-mapkubeapis --version v0.2.0
 
 echo -e "\nHelm Plugins:"
 helm plugin list
